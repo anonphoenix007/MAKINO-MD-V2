10a96353b471e26c39c97abf4319e2898876d806
Freddy Phoenix Mills <118795606+anonphoenix007@users.noreply.github.com>
2024-05-10 20:10:48 +0100

    Update MAKINO-MD-V2.js

--- MAKINO-MD-V2.js
+++ MAKINO-MD-V2.js
@@ -7848,7 +7848,8 @@ ${readmore}
 └───────────────···▸▸▸`
 	    let buttonMessage = {
 	    //audio: { url: "./Assets/slay.mp3" }, mimetype: 'audio/mp4',
-	    image: fs.readFileSync("./Assets/pic7.jpg"),
+	    //image: fs.readFileSync("./Assets/pic7.jpg"),
+	    image: { url: },
             caption: helpmenu,
             headerType: 4,
             contextInfo: {
