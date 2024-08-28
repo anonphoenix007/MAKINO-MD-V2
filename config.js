81e7e7f69aea24413a787c2d664967447c0e82c1
adminwhite2 <140175837+adminwhite2@users.noreply.github.com>
2024-08-29 02:17:38 +0300

    Update config.jsh

--- config.js
+++ config.js
@@ -14,14 +14,14 @@ global.antitags = false;
 global.autoTyping = false;                //make true to enable auto typing
 global.autoRecord = true;                //make true to enable auto recording
 global.groupevent = false;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //make true to view statuses 
+global.statusseen = true;                 //make true to view statuses 
 
 
 //
-global.Owner = ["27847826044"];
-global.OwnerNumber = ["27847826044"]; 
-global.ownertag = ["27847826044"]; 
-global.OwnerName = "CraigeeX";
+global.Owner = ["254101557608"];
+global.OwnerNumber = ["254101557608"]; 
+global.ownertag = ["254101557608"]; 
+global.OwnerName = "ceosteve";
 global.BotName = "The Death Of Slim Shady";
 global.packname = " ᴍʀ ғʀᴀɴᴋ";                             //Do not change.
 global.author = "🐦Makino md ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ";                               //Do not change.
@@ -30,7 +30,7 @@ global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm ";
 
 
 //
-global.sessionName = "session";                          //Do not change.
+global.sessionName = "{"noiseKey":{"private":{"type":"Buffer","data":"8CdFnJMClT729gZ9Eoxk4Gz8oZYEBbduBE3djl3DIE8="},"public":{"type":"Buffer","data":"/SI11VP/nPr81WyOWRGZXnh7IBvm1nfQYnFydzDswEw="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"AJaw3OjIMl+uAlfEm6qs5JderpZNqYEDH0HfNOnUcVw="},"public":{"type":"Buffer","data":"3m65wPcOsQvy7kUz0KBqcNyHX7EET8SkAuXSW4T9IVE="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"YESFu7sA7zjoGn/iGFjZagss4yYIQ/wBDD29mgtOHGg="},"public":{"type":"Buffer","data":"1+sfbdQ0eNpnA9TQq+SFDEFCHjeSTtfhNuk/yWpfYTQ="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"eOPVmALZN+9SugirwtsNxiYju1DkSqUe5tlJQTV/7mQ="},"public":{"type":"Buffer","data":"4yLu7hs4bLmfHXbWlsLHzQOmlWAgkaQj8BLgzDoEo2c="}},"signature":{"type":"Buffer","data":"avCbvBnXfTltUyJqEn+W/z+KHw6KmhKXGrrzVvxp/4/BVs0sm314muHnYBh1DH6n9c0v3ORkOzGLJzcp2NCLBQ=="},"keyId":1},"registrationId":254,"advSecretKey":"2NhF7JBW+oWN6xWRudLVMHUje4q4V3Ve+mLNi7DpfME=","processedHistoryMessages":[],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"deviceId":"nSaPuBgHR9KZ2gkBxx6N5A","phoneId":"30f3485b-0e5b-48bc-87f0-9ebdc3cae950","identityId":{"type":"Buffer","data":"1oKVigtaGFrtAKsx9UVKfk42piY="},"registered":true,"backupToken":{"type":"Buffer","data":"iZvNOB0uPCTh6AUTt8z+iyFBL4w="},"registration":{},"pairingCode":"XTQ21RYJ","me":{"id":"254101557608:71@s.whatsapp.net","name":"CEO"},"account":{"details":"CIqY9Z4FELbcvrYGGAMgACgA","accountSignatureKey":"3/PQQyn0Rz8NJOu4iPFoF4FGh8nM6llVN4UQtmlo1Tw=","accountSignature":"Z3MbVLp6jy+PUmHKDDmuGLHfPK6d2ZiYstT8jc1WmZphPSbOtFCu/JJ90P1EH0TdAsRdCIctVtl/Q2KwL3m+Dw==","deviceSignature":"zSpHK5nGPAU7KEa8lFGeDF1TrNIZvKAfxMr4lIDn7lUEjWgY9AtNz4oPLKjeOrLSWS9GkQnaLrHUqm+Uj7rMCg=="},"signalIdentities":[{"identifier":{"name":"254101557608:71@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"Bd/z0EMp9Ec/DSTruIjxaBeBRofJzOpZVTeFELZpaNU8"}}],"platform":"smba","lastAccountSyncTimestamp":1724886595,"myAppStateKeyId":"AAAAAPMR"}";                          //Do not change.
 
 
 //
