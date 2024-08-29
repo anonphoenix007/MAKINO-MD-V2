ff286f298adde52af62e6e920c7914bac1328588
Taira Makino <118795606+anonphoenix007@users.noreply.github.com>
2024-08-29 08:03:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.ANTI_BOT = false;
 global.PM_BLOCKER = false;
 global.CHATBOT = false;
 
-global.SESSION_ID = process.env.SESSION_ID || ""
-global.Owner = ["2348074439626"]; //like 2347080968564 
+global.SESSION_ID = process.env.SESSION_ID || "taira_tech-QQc5JESx"
+global.Owner = ["2348138686232"]; //like 2347080968564 
 global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
 global.BotName = "♱MAKINO-MD-V2♱♡⃤";
 global.packname = "♱MAKINO-MD-V2♱♡⃤";                             //Do not change.
