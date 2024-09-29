4b021d2698da678082c584d1c71e22eeea3c2bf4
Taira Makino <118795606+anonphoenix007@users.noreply.github.com>
2024-09-29 11:52:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.ANTI_BOT = false;
 global.PM_BLOCKER = false;
 global.CHATBOT = false;
 
-global.SESSION_ID = process.env.SESSION_ID || "taira_tech-7xlG3aSa#i_k7EeNinW4spdmBjDNUaGB_4_FrX2uApuAgtzEfyRY"
+global.SESSION_ID = process.env.SESSION_ID || ""
 global.Owner = ["2349151864541"]; //like 2347080968564 
 global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
 global.BotName = "♱MAKINO-MD-V2♱♡⃤";
